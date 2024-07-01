8e5c09e9bd081df7f08ba47c54f200f299a3365f
andedrag <andedrag@gmail.com>
2024-07-01 09:46:43 +0200

    chore: Controller to kotlin

--- src/main/java/hu/telekom/tkriszti/flightInfo/controller/AppController.kt
+++ src/main/java/hu/telekom/tkriszti/flightInfo/controller/AppController.kt
@@ -1,36 +1,26 @@
-package hu.telekom.tkriszti.flightInfo.controller;
+package hu.telekom.tkriszti.flightInfo.controller
 
-import java.sql.SQLException;
-import java.util.Set;
-
-import hu.telekom.tkriszti.flightInfo.dto.ResultDTO;
-import hu.telekom.tkriszti.flightInfo.service.PilotInfoServiceDbImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import hu.telekom.tkriszti.flightInfo.dto.ResultDTO
+import hu.telekom.tkriszti.flightInfo.service.PilotInfoServiceDbImpl
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import java.sql.SQLException
+import kotlin.jvm.Throws
 
 @Controller
-public class AppController {
-	
-	private final PilotInfoServiceDbImpl service;
+class AppController {
+    @Autowired
+    fun constructor(service: PilotInfoServiceDbImpl) {
 
-	@Autowired
-	public AppController(PilotInfoServiceDbImpl service) {
-		super();
-		this.service = service;
-	}
-
-	// localhost:8080/pilot/kapitanyjanos --> pathvariable-ös annotáció
-	// localhost:8080/pilot?name=kapitanyjanos --> @RequestParam
-	@GetMapping("/pilot")
-	public String showPilotData(
-			Model model,
-			@RequestParam("name") String name
-			) throws SQLException {
-		Set<ResultDTO> pilotDtos = service.getPilotData(name);
-		model.addAttribute("pilot", pilotDtos);
-		return "pilot.html";
-	}
+        @GetMapping("/pilot")
+        //@Throws(SQLException::class)
+        fun showPilotData(model: Model, @RequestParam("name") name: String): String {
+            var pilotDtos: Set<ResultDTO> = service.getPilotData(name)
+            model.addAttribute("pilot", pilotDtos)
+            return "pilot.html"
+        }
+    }
 }
