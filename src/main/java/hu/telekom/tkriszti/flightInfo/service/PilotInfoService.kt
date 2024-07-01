702238e34b46d93937c99eadbf73410d7da25331
andedrag <andedrag@gmail.com>
2024-07-01 14:16:14 +0200

    chore: convert service to Kotlin

--- src/main/java/hu/telekom/tkriszti/flightInfo/service/PilotInfoService.kt
+++ src/main/java/hu/telekom/tkriszti/flightInfo/service/PilotInfoService.kt
@@ -1,10 +1,9 @@
-package hu.telekom.tkriszti.flightInfo.service;
+package hu.telekom.tkriszti.flightInfo.service
 
-import hu.telekom.tkriszti.flightInfo.dto.ResultDTO;
-
-import java.sql.SQLException;
-import java.util.Set;
+import hu.telekom.tkriszti.flightInfo.dto.ResultDTO
+import java.sql.SQLException
 
 interface PilotInfoService {
-    Set<ResultDTO> getPilotData(String pilotName) throws SQLException;
+    @Throws(SQLException::class)
+    fun getPilotData(pilotName: String?): Set<ResultDTO?>?
 }
--- src/main/java/hu/telekom/tkriszti/flightInfo/service/PilotInfoServiceDbImpl.kt
+++ src/main/java/hu/telekom/tkriszti/flightInfo/service/PilotInfoServiceDbImpl.kt
@@ -1,60 +1,43 @@
-package hu.telekom.tkriszti.flightInfo.service;
+package hu.telekom.tkriszti.flightInfo.service
 
-import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import hu.telekom.tkriszti.flightInfo.dao.DataAccess;
-import hu.telekom.tkriszti.flightInfo.dto.ResultDTO;
-import hu.telekom.tkriszti.flightInfo.model.Flight;
-import hu.telekom.tkriszti.flightInfo.model.Pilot;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import hu.telekom.tkriszti.flightInfo.dao.DataAccess
+import hu.telekom.tkriszti.flightInfo.dto.ResultDTO
+import hu.telekom.tkriszti.flightInfo.model.Flight
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.sql.SQLException
+import java.util.*
+import java.util.function.Supplier
+import java.util.stream.Collectors
 
 @Service
-public class PilotInfoServiceDbImpl implements PilotInfoService {
-	
-	private final DataAccess dao;
-	
-	@Autowired
-	public PilotInfoServiceDbImpl(DataAccess dao) {
-		this.dao = dao;
-	}
+class PilotInfoServiceDbImpl @Autowired constructor(private val dao: DataAccess) : PilotInfoService {
+    @Throws(SQLException::class)
+    override fun getPilotData(pilotName: String?): Set<ResultDTO?> {
+        val relevantPilotData: MutableSet<ResultDTO?> = HashSet()
+        val pilots = dao.getPilotByName(pilotName)
+        for (pilot in pilots) {
+            val licenseYear = pilot!!.getLicenseYear()
+            val flights = dao.getFlightsByPilotId(pilot.getId())
+            val flightIds: List<Int> = flights.stream().map { obj: Flight? -> obj!!.getID() }.collect(
+                Collectors.toCollection(
+                    Supplier { LinkedList() })
+            )
+            val sumOfFlightTime = getSumOfFlightTime(flights)
+            relevantPilotData.add(
+                ResultDTO(pilotName, licenseYear, flightIds, sumOfFlightTime)
+            )
+        }
+        return relevantPilotData
+    }
 
-	@Override
-	public Set<ResultDTO> getPilotData(String pilotName) throws SQLException {
-		Set<ResultDTO> relevantPilotData = new HashSet<>();
-		Set<Pilot> pilots = dao.getPilotByName(pilotName);
-		for (Pilot pilot : pilots) {
-			int licenseYear = pilot.getLicenseYear();
-			Set<Flight> flights = dao.getFlightsByPilotId(pilot.getId());
-			List<Integer> flightIds = flights.stream().map(Flight::getID).collect(Collectors.toCollection(LinkedList::new));
-			int sumOfFlightTime = getSumOfFlightTime(flights);
-			relevantPilotData.add(
-					new ResultDTO(pilotName, licenseYear, flightIds, sumOfFlightTime));
-		}
-		return relevantPilotData;
-	}
-
-	private static int getSumOfFlightTime(Set<Flight> flights) {
-		int sumOfFlightTime = 0;
-		for (Flight flight : flights) {
-			sumOfFlightTime +=flight.getFlightTime();
-		}
-		return sumOfFlightTime;
-	}
+    companion object {
+        private fun getSumOfFlightTime(flights: Set<Flight?>): Int {
+            var sumOfFlightTime = 0
+            for (flight in flights) {
+                sumOfFlightTime += flight!!.getFlightTime()
+            }
+            return sumOfFlightTime
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
