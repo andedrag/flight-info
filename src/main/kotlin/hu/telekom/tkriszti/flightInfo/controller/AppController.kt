52ac5d35a7420e9222d62556fd4a5e43bed8912a
andedrag <andedrag@gmail.com>
2024-07-14 01:38:42 +0200

    chore: cleanup Controller, DAO

--- src/main/kotlin/hu/telekom/tkriszti/flightInfo/controller/AppController.kt
+++ src/main/kotlin/hu/telekom/tkriszti/flightInfo/controller/AppController.kt
@@ -1,16 +1,13 @@
 package hu.telekom.tkriszti.flightInfo.controller
 
-import hu.telekom.tkriszti.flightInfo.dao.DataAccess
 import hu.telekom.tkriszti.flightInfo.dto.ResultDTO
 import hu.telekom.tkriszti.flightInfo.service.PilotInfoService
-import hu.telekom.tkriszti.flightInfo.service.PilotInfoServiceDbImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import java.sql.SQLException
-import kotlin.jvm.Throws
 
 @Controller
 class AppController
--- src/main/kotlin/hu/telekom/tkriszti/flightInfo/dao/DataAccess.kt
+++ src/main/kotlin/hu/telekom/tkriszti/flightInfo/dao/DataAccess.kt
@@ -3,42 +3,32 @@ package hu.telekom.tkriszti.flightInfo.dao
 import hu.telekom.tkriszti.flightInfo.model.Flight
 import hu.telekom.tkriszti.flightInfo.model.Pilot
 import org.hibernate.SessionFactory
-import org.hibernate.cfg.Configuration
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Repository
-import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.sql.SQLException
 
 @Component
 //@Repository
 open class DataAccess (@Autowired private val sessionFactory: SessionFactory) {
 
+    @Transactional
     @Throws(SQLException::class)
-    fun getPilotByName(pilotName: String?): Set<Pilot?> {
-        /** Open Session and Begin Transaction  */
+    fun getPilotByName(pilotName: String): Set<Pilot?> {
         val session = sessionFactory.openSession()
-        val transaction = session?.beginTransaction()
-        /** Perform SELECT operation  */
-        val query = session?.createQuery("FROM Pilot P WHERE P.name = :pilotname")
-        query!!.setParameter("pilotname", pilotName)
-        val relevantPilots: HashSet<Pilot> = HashSet(query.list()?.filterIsInstance<Pilot>()?.let { HashSet(it) } ?: HashSet()) // Ezt lehetne egyszerűbben is?
-        /** Execute transaction and Close Session  */
-        transaction?.commit()
-        session.close()
+        val query = session.createQuery("FROM Pilot P WHERE P.name = :pilotname")
+        query.setParameter("pilotname", pilotName)
+        val relevantPilots: HashSet<Pilot?> = HashSet(query.list().filterIsInstance<Pilot>().let { HashSet(it) } )
         return relevantPilots
     }
 
+    @Transactional
     @Throws(SQLException::class)
     fun getFlightsByPilotId(pilotId: Int): Set<Flight?> {
         val session = sessionFactory.openSession()
-        val transaction = session.beginTransaction()
         val query = session.createQuery("FROM Flight F WHERE pilot1Id = :pilotid OR pilot2Id = :pilotid")
         query.setParameter("pilotid", pilotId)
-        val relevantFlights: Set<Flight> = HashSet(query.list()?.filterIsInstance<Flight>()?.let { HashSet(it) } ?: HashSet())
-        transaction.commit()
-        session.close()
+        val relevantFlights: Set<Flight> = HashSet(query.list().filterIsInstance<Flight>().let { HashSet(it) } )
         return relevantFlights
     }
 }
