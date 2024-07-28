b082320f3fee1f6bc62e9135ff51580dfebd1ccd
andedrag <andedrag@gmail.com>
2024-07-28 21:04:03 +0200

    chore: clean

--- src/main/kotlin/hu/telekom/tkriszti/flightInfo/service/PilotInfoServiceDbImpl.kt
+++ src/main/kotlin/hu/telekom/tkriszti/flightInfo/service/PilotInfoServiceDbImpl.kt
@@ -14,24 +14,17 @@ class PilotInfoServiceDbImpl (@Autowired private val dao: DataAccess) : PilotInf
     @Throws(SQLException::class)
     override fun getPilotData(pilotName: String): Set<ResultDTO?> {
         val relevantPilotData: MutableSet<ResultDTO?> = HashSet()
-       // val pilots = dao.getPilotByName(pilotName)
         val pilots = dao.findByName(pilotName)
         for (pilot in pilots) {
-          //  if (pilot != null) {
-               // val licenseYear = pilot.getLicenseYear()
-            val licenseYear = pilot.licenseYear
-                //val flights = dao.getFlightsByPilotId(pilot.getId())
-            val flights = dao.findByPilot1IdOrPilot2Id(pilot.id)
-//            val flightIds: List<Int> = flights.stream().map { obj: Flight? -> obj?.getID() }.collect(
-//                    Collectors.toCollection { LinkedList() }
-            val flightIds: List<String> = flights.stream().map { obj: Flight? -> obj?.id }.collect(
+        val licenseYear = pilot.licenseYear
+        val flights = dao.findByPilot1IdOrPilot2Id(pilot.id)
+        val flightIds: List<String> = flights.stream().map { obj: Flight? -> obj?.id }.collect(
                 Collectors.toCollection { LinkedList() }
                 )
-                val sumOfFlightTime = getSumOfFlightTime(flights)
+        val sumOfFlightTime = getSumOfFlightTime(flights)
                 relevantPilotData.add(
                     ResultDTO(pilotName, licenseYear, flightIds, sumOfFlightTime)
                 )
-          //  }
         }
         return relevantPilotData
     }
@@ -41,8 +34,7 @@ class PilotInfoServiceDbImpl (@Autowired private val dao: DataAccess) : PilotInf
             var sumOfFlightTime = 0
             for (flight in flights) {
                 if (flight != null)
-                // sumOfFlightTime += flight.getFlightTime()
-                    sumOfFlightTime += flight.flightTime
+                   sumOfFlightTime += flight.flightTime
             }
             return sumOfFlightTime
         }
