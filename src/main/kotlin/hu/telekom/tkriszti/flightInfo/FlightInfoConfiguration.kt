bebfc78b6a5391de33656201917f7fe7add5be52
andedrag <andedrag@gmail.com>
2024-08-26 11:03:42 +0200

    chore: remove sql-related codes

--- src/main/kotlin/hu/telekom/tkriszti/flightInfo/FlightInfoConfiguration.kt
+++ src/main/kotlin/hu/telekom/tkriszti/flightInfo/FlightInfoConfiguration.kt
@@ -1,30 +1,30 @@
-package hu.telekom.tkriszti.flightInfo
-
-import org.hibernate.SessionFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-open class FlightInfoConfiguration {
-
-
-    @Value("\${spring.datasource.url}")
-    lateinit var dbUrl: String
-
-    @Value("\${spring.datasource.username}")
-    lateinit var dbUsername: String
-
-    @Value("\${spring.datasource.password}")
-    lateinit var dbPassword: String
-
-    @Bean
-    open fun createSessionFactory() : SessionFactory {
-        val config = org.hibernate.cfg.Configuration()
-        config.configure() // load settings from hibernate.cfg.xml
-        config.setProperty("hibernate.connection.url", dbUrl)
-        config.setProperty("hibernate.connection.username", dbUsername)
-        config.setProperty("hibernate.connection.password", dbPassword)
-        return config.buildSessionFactory()
-    }
-}
+//package hu.telekom.tkriszti.flightInfo
+//
+//import org.hibernate.SessionFactory
+//import org.springframework.beans.factory.annotation.Value
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//
+//@Configuration
+//open class FlightInfoConfiguration {
+//
+//
+//    @Value("\${spring.datasource.url}")
+//    lateinit var dbUrl: String
+//
+//    @Value("\${spring.datasource.username}")
+//    lateinit var dbUsername: String
+//
+//    @Value("\${spring.datasource.password}")
+//    lateinit var dbPassword: String
+//
+//    @Bean
+//    open fun createSessionFactory() : SessionFactory {
+//        val config = org.hibernate.cfg.Configuration()
+//        config.configure() // load settings from hibernate.cfg.xml
+//        config.setProperty("hibernate.connection.url", dbUrl)
+//        config.setProperty("hibernate.connection.username", dbUsername)
+//        config.setProperty("hibernate.connection.password", dbPassword)
+//        return config.buildSessionFactory()
+//    }
+//}
